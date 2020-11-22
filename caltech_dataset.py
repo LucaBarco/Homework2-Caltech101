b2736f68d511ef40535dac5fe298e9d209852c2e
Luca Barco <s276072@studenti.polito.it>
2020-11-22 16:54:30 +0100

    fix paths

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -33,7 +33,7 @@ class Caltech(VisionDataset):
         we can create a list of (id, image, idLabel)
         we can create a map of (idLabel, label)
         '''
-        print(os.getcwd())
+        get_ipython().system(" pwd")
         file = open('Caltech101/' + split + '.txt', "r")
 
         lines = file.readlines()
