25cc0876d2f9162d104e62678e84a3c70214be8a
Luca Barco <s276072@studenti.polito.it>
2020-11-22 17:44:44 +0100

    fix

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -44,6 +44,8 @@ class Caltech(VisionDataset):
 
             if "BACKGROUND_Google" not in line:
                 label_name = line.split('/')[0]
+                if label_name not in self.labels.key():
+                    self.labels[label_name]=[]
                 self.labels[label_name] = self.labels[label_name].append(count_images)
                 self.images[count_images] = (pil_loader(root + '/' + line[:-1]), self.labels[label_name])
                 count_images = count_images + 1
