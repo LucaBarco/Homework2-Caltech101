cad712dc0dba5f3f16c05c71f33df6f43adc8fd8
Luca Barco <s276072@studenti.polito.it>
2020-11-22 16:45:21 +0100

    fix paths

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -17,7 +17,7 @@ def pil_loader(path):
 class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
-
+        self.root=root
         self.split = split # This defines the split you are going to use
                            # (split files are called 'train.txt' and 'test.txt')
 
@@ -33,8 +33,9 @@ class Caltech(VisionDataset):
         we can create a list of (id, image, idLabel)
         we can create a map of (idLabel, label)
         '''
+
         print('Caltech101' + '/' + split + '.txt')
-        file = open('Caltech101'+'/'+split+'.txt', "r")
+        file = open('./Caltech101'+'/'+split+'.txt', "r")
 
         lines = file.readlines()
         self.images = {}
